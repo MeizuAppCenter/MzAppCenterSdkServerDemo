21c59b5c9efb59fd7fc4a885fd714ceab8cb7ef4
1160135186 <qq1160135172@163.com>
2019-09-26 20:04:22 +0800

    Create ResponseErrorCode.java

---
+++ Java/ResponseErrorCode.java
@@ -0,0 +1,19 @@
+package com.meizu.lichee.api.action;
+
+/**
+ * 异常code 可自行定义
+ */
+public interface ResponseErrorCode {
+
+	int START = 211000;
+
+	int END = 211999;
+
+	/** 不合法*/
+	int APP_SIGN_ERROR = START + 1;
+
+	/**
+	 * 订单状态不正确
+	 */
+	int STATUS_NOT_CORRECT = START + 2;
+}
